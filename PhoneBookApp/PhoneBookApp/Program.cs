8e6c0ffb00f7e96b7a71625eca48a74e47ea3d40
mateaW <matea.vucemilovic@gmail.com>
2023-11-25 22:03:47 +0100

    Add main menu

--- PhoneBookApp/PhoneBookApp/Program.cs
+++ PhoneBookApp/PhoneBookApp/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace PhoneBookApp
 {
@@ -27,6 +28,57 @@ namespace PhoneBookApp
             phoneBook.Add(contact5, new List<Call>());
             phoneBook.Add(contact6, new List<Call>());
             phoneBook.Add(contact7, new List<Call>());
+
+            while (true)
+            {
+                Console.WriteLine("MENU: ");
+                Console.WriteLine("1 - Ispis svih kontakata");
+                Console.WriteLine("2 - Dodavanje novih kontakata u imenik");
+                Console.WriteLine("3 - Brisanje kontakata iz imenika");
+                Console.WriteLine("4 - Uređivanje preference kontakta");
+                Console.WriteLine("5 - Upravljanje kontaktom");
+                Console.WriteLine("6 - Ispis svih poziva");
+                Console.WriteLine("7 - Izlaz iz aplikacije");
+
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Option 1");
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Option 2");
+                        break;
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine("Option 3");
+                        break;
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("Option 4");
+                        break;
+                    case "5":
+                        Console.Clear();
+                        Console.WriteLine("Option 5");
+                        break;
+                    case "6":
+                        Console.Clear();
+                        Console.WriteLine("Option 6");
+                        break;
+                    case "7":
+                        Console.Clear();
+                        Console.WriteLine("Izlaz iz aplikacije..");
+                        Thread.Sleep(1000);
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Pogrešan unos. Upišite brojeve 1-7.");
+                        break;
+                }
+            }
         }
     }
 }
