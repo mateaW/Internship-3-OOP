8157483a77b98ddececbb24eb130d4c4a15e24ac
mateaW <matea.vucemilovic@gmail.com>
2023-11-26 00:40:14 +0100

    Add method to delete contacts

--- PhoneBookApp/PhoneBookApp/Program.cs
+++ PhoneBookApp/PhoneBookApp/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.ComponentModel.Design;
 
 namespace PhoneBookApp
 {
@@ -14,9 +15,9 @@ namespace PhoneBookApp
         static void Main(string[] args)
         {
             Contact contact1 = new Contact("Marin", "Cecić", "091 234 5678", Prefference.Favourite);
-            Contact contact2 = new Contact("Marta", "Katić", "095 539 7809", Prefference.Normal);
+            Contact contact2 = new Contact("Marta", "Batinić", "095 539 7809", Prefference.Normal);
             Contact contact3 = new Contact("Rina", "Miočić", "091 329 1312", Prefference.Normal);
-            Contact contact4 = new Contact("Marta", "Katić", "099 530 7889", Prefference.Normal);
+            Contact contact4 = new Contact("Mladen", "Katić", "099 530 7889", Prefference.Normal);
             Contact contact5 = new Contact("Stipe", "Bilonić", "092 234 7799", Prefference.Favourite);
             Contact contact6 = new Contact("Marta", "Katić", "091 100 4429", Prefference.Blocked);
             Contact contact7 = new Contact("Paula", "Vranjić", "091 3463 977", Prefference.Normal);
@@ -57,7 +58,7 @@ namespace PhoneBookApp
                         break;
                     case "3":
                         Console.Clear();
-                        Console.WriteLine("Option 3");
+                        DeleteContact();
                         break;
                     case "4":
                         Console.Clear();
@@ -117,10 +118,13 @@ namespace PhoneBookApp
         static void PrintAllContacts()
         {
             Console.WriteLine("----SVI KONTAKTI----");
-            foreach (var contact in phoneBook.Keys)
+            
+            for(int i = 0; i < phoneBook.Count(); i++)
             {
-                Console.WriteLine($"{contact.FirstName} {contact.LastName} - {contact.PhoneNumber} - {contact.Prefference}");
+                Contact contact = phoneBook.Keys.ElementAt(i);
+                Console.WriteLine($"{i+1}. {contact.FirstName} {contact.LastName} - {contact.PhoneNumber} - {contact.Prefference}");
             }
+             
             Console.WriteLine();
             Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni...");
             Console.ReadKey();
@@ -229,6 +233,61 @@ namespace PhoneBookApp
                     continue;
                 }
                 break;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni: ");
+            Console.ReadKey();
+        }
+
+        static void DeleteContact()
+        {
+            Console.WriteLine("----BRISANJE KONTAKTA----");
+
+            // prikaz kontakata da korisnik moze vidjeti koji obrisati
+            for (int i = 0; i < phoneBook.Count(); i++)
+            {
+                Contact contact = phoneBook.Keys.ElementAt(i);
+                Console.WriteLine($"{i + 1}. {contact.FirstName} {contact.LastName} - {contact.PhoneNumber} - {contact.Prefference}");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Unesite ime kontakta koji se briše: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Unesite prezime kontakta koji se briše: ");
+            string lastName = Console.ReadLine();
+
+            Contact contactToDelete = phoneBook.Keys.FirstOrDefault(k => k.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase) &&
+            k.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase));
+
+            if (contactToDelete != null)
+            {
+                while (true)
+                {
+                    Console.WriteLine($"Potvrđujete li brisanje kontakta {firstName} {lastName} (da/ne)?");
+                    string conf = Console.ReadLine();
+
+                    if (conf.ToLower() == "da")
+                    {
+                        phoneBook.Remove(contactToDelete);
+                        Console.WriteLine($"Kontakt {firstName} {lastName} izbrisan iz imenika.");
+                    }
+                    else if (conf.ToLower() == "ne")
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pogrešan unos.");
+                        continue;
+                    }
+                    break;
+                } 
+            }
+            else
+            {
+                Console.WriteLine("Kontakt nije pronađen.");
             }
             Console.WriteLine();
             Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni: ");
