c8b0f0727df8b2d23c504b1ba5aff6c85178933c
mateaW <matea.vucemilovic@gmail.com>
2023-11-25 21:15:19 +0100

    Add classes and enums

---
+++ PhoneBookApp/PhoneBookApp/Classes/Call.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoneBookApp.Enums;
+
+namespace PhoneBookApp
+{
+    public class Call
+    {
+        public DateTime CallTime { get; set; }
+        public Status Status { get; set; }
+
+        public Call(DateTime callTime, Status status)
+        {
+            CallTime = callTime;
+            Status = status;
+        }
+    }
+}
---
+++ PhoneBookApp/PhoneBookApp/Classes/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoneBookApp.Enums;
+
+namespace PhoneBookApp
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Prefference Prefference { get; set; }
+
+        public Contact(string firstName, string lastName, string phoneNumber, Prefference prefference)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Prefference = prefference;
+        }
+    }
+}
