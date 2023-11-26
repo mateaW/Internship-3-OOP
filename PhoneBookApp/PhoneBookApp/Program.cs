a328e068b96fd781b98e5edb7bc7a02e027b3d9b
mateaW <matea.vucemilovic@gmail.com>
2023-11-26 15:54:57 +0100

    Add calls with each contact in main

--- PhoneBookApp/PhoneBookApp/Program.cs
+++ PhoneBookApp/PhoneBookApp/Program.cs
@@ -14,23 +14,45 @@ namespace PhoneBookApp
         static Dictionary<Contact, List<Call>> phoneBook = new Dictionary<Contact, List<Call>>();
         static void Main(string[] args)
         {
+            // prvi kontakt i svi pozivi s tim kontaktom
             Contact contact1 = new Contact("Marin", "Cecić", "091 234 5678", Prefference.Favourite);
+            List<Call> calls1 = new List<Call>();
+            Call contact1call1 = new Call(new DateTime(), Status.Finished);
+            Call contact1call2 = new Call(new DateTime(), Status.Finished);
+            calls1.Add(contact1call1); 
+            calls1.Add(contact1call2);
+            phoneBook.Add(contact1, calls1);
+            //
+
             Contact contact2 = new Contact("Marta", "Batinić", "095 539 7809", Prefference.Normal);
+            List<Call> calls2 = new List<Call>();
+            Call contact2call1 = new Call(new DateTime(), Status.Missed);
+            calls2.Add(contact2call1);
+            phoneBook.Add(contact2, calls2);
+
             Contact contact3 = new Contact("Rina", "Miočić", "091 329 1312", Prefference.Normal);
-            Contact contact4 = new Contact("Mladen", "Katić", "099 530 7889", Prefference.Normal);
-            Contact contact5 = new Contact("Stipe", "Bilonić", "092 234 7799", Prefference.Favourite);
-            Contact contact6 = new Contact("Marta", "Katić", "091 100 4429", Prefference.Blocked);
-            Contact contact7 = new Contact("Paula", "Vranjić", "091 3463 977", Prefference.Normal);
+            List<Call> calls3 = new List<Call>();
+            Call contact3call1 = new Call(new DateTime(), Status.InProgress);
+            Call contact3call2 = new Call(new DateTime(), Status.Finished);
+            Call contact3call3 = new Call(new DateTime(), Status.Finished);
+            calls3.Add(contact3call1);
+            calls3.Add(contact3call2);
+            calls3.Add(contact3call3);
+            phoneBook.Add(contact3, calls3);
 
-            phoneBook.Add(contact1, new List<Call>());
-            phoneBook.Add(contact2, new List<Call>());
-            phoneBook.Add(contact3, new List<Call>());
-            phoneBook.Add(contact4, new List<Call>());
-            phoneBook.Add(contact5, new List<Call>());
-            phoneBook.Add(contact6, new List<Call>());
-            phoneBook.Add(contact7, new List<Call>());
+            Contact contact4 = new Contact("Stipe", "Bilonić", "092 234 7799", Prefference.Favourite);
+            List<Call> calls4 = new List<Call>();
+            Call contact4call1 = new Call(new DateTime(), Status.Missed);
+            Call contact4call2 = new Call(new DateTime(), Status.Finished);
+            calls4.Add(contact4call1);
+            calls4.Add(contact4call2);
+            phoneBook.Add(contact4, calls4);
 
-            
+            Contact contact5 = new Contact("Marta", "Katić", "091 100 4429", Prefference.Blocked);
+            List<Call> calls5 = new List<Call>();
+            Call contact5call1 = new Call(new DateTime(), Status.InProgress);
+            calls5.Add(contact5call1);
+            phoneBook.Add(contact5, calls5);
 
             while (true)
             {
@@ -126,7 +148,7 @@ namespace PhoneBookApp
             }
              
             Console.WriteLine();
-            Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni...");
+            Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni... ");
             Console.ReadKey();
         }
 
@@ -235,7 +257,7 @@ namespace PhoneBookApp
                 break;
             }
             Console.WriteLine();
-            Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni: ");
+            Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni... ");
             Console.ReadKey();
         }
 
@@ -290,7 +312,7 @@ namespace PhoneBookApp
                 Console.WriteLine("Kontakt nije pronađen.");
             }
             Console.WriteLine();
-            Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni: ");
+            Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni... ");
             Console.ReadKey();
         }
         static void EditPrefferences()
@@ -373,7 +395,7 @@ namespace PhoneBookApp
                 Console.WriteLine("Kontakt nije pronađen.");
             }
             Console.WriteLine();
-            Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni: ");
+            Console.WriteLine("Pritisnite bilo koju tipku za povratak na glavni meni... ");
             Console.ReadKey();
         }
         static void PrintAllCalls()
